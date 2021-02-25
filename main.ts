08c866b9a0d48c0dc0cfbf27bcfd1f358e138834
bryan weinstein <bryanweinstein@outlook.com>
2021-02-25 13:02:20 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,17 @@ let Counter = 0
 basic.forever(function () {
     while (input.temperature() > 20) {
         basic.showIcon(IconNames.Angry)
-        Counter = 4
+        Counter += 1
+        if (Counter == 0) {
+            basic.showIcon(IconNames.Heart)
+        }
     }
-    if (input.compassHeading() > 180) {
-        basic.showIcon(IconNames.Heart)
-        music.playTone(262, music.beat(BeatFraction.Whole))
+    while (input.compassHeading() > 180) {
+        for (let index = 0; index <= 4; index++) {
+            basic.showIcon(IconNames.Heart)
+            music.playTone(262, music.beat(BeatFraction.Eighth))
+            music.stopAllSounds()
+            music.playTone(440, music.beat(BeatFraction.Eighth))
+        }
     }
 })
